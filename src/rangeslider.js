3af06441e115e7f1ba641c30b283442a7ab2edea
RostislavIvlev <56726375+RostislavIvlev@users.noreply.github.com>
2020-11-24 17:37:34 +0500

    function rsCalculate function showInput changes in rsBuild

--- src/rangeslider.js
+++ src/rangeslider.js
@@ -7,8 +7,9 @@
                 thumbPosition:        0,
                 additionPosition:     100, 
                 min:                  0,
-                max:                  0,
+                max:                  100,
                 description:          false,
+                display:              false,
             }
 
             var settings = $.extend({}, this.defaultOptions, options);
@@ -17,37 +18,48 @@
                 let _this = this;
                 _this.rsWrapper = $("<div/>").appendTo(settings.target);
                 _this.rsWrapper.addClass("rsWrapper");
+
+                if (settings.display == true) {
+                    _this.rsDisplay = $("<input/>").appendTo(rsWrapper);
+                    _this.rsDisplay.addClass("rsDisplay");
+                };
             
                 _this.rsBase = $("<div/>").appendTo(rsWrapper);
                 _this.rsBase.addClass("rsBase");
             
                 _this.rsThumb1 = $("<div/>").appendTo(rsBase);
                 _this.rsThumb1.addClass("rsThumb rsThumb_1");
-                _this.rsThumb1.css("left", settings.thumbPosition)
+                _this.rsThumb1.css("left", rsCalculate().thumbPosition1);
 
             
                 if (settings.additionThumb == true) {
                     _this.rsThumb2 = $("<div/>").appendTo(rsBase);
                     _this.rsThumb2.addClass("rsThumb rsThumb_2");
-                    _this.rsThumb2.css("left", settings.additionPosition)
-                }
-            }
+                    _this.rsThumb2.css("left", rsCalculate().thumbPosition2);
+                };
+            };
 
-            function rsModel() {
+            function rsCalculate() {
+                let baseWidth = rsBase.css("width").slice(0, -2);
+                let minMax = settings.max - settings.min;
+                let thumbPosition1 = (baseWidth/minMax * settings.thumbPosition) - 6;
+                let thumbPosition2 = baseWidth/minMax * settings.additionPosition - 6;
 
-            }
+                return {
+                    thumbPosition1,
+                    thumbPosition2
+                };
+            };
 
-            function rsConstructor() {
-                let baseWidth = rsBase.css("width");
-                
+            function showInput() {
+                rsDisplay.val(settings.thumbPosition + " - " + settings.additionPosition);
             }
 
             return this.each(function() {
                 var $this = $(this);
-                
-                rsBuild($this);
-                rsModel($this);
-                rsConstructor($this);
+
+                rsBuild();
+                showInput();
             });
         }
     });
@@ -56,6 +68,9 @@
 
 var $rangeslider = $(".test").rangeslider({
     additionThumb: true,
-    thumbPosition: "10px",
-    additionPosition: "100px",
+    thumbPosition: 5000,
+    additionPosition: 10000,
+    display: true,
+    min: 0,
+    max: 15000,
 })
