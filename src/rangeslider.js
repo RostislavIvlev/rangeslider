6f80909fedc90c6cd631c3b71a121676d54b1406
RostislavIvlev <56726375+RostislavIvlev@users.noreply.github.com>
2020-11-24 14:08:51 +0500

    Settings, options extend, default options, MVP "structure"

--- src/rangeslider.js
+++ src/rangeslider.js
@@ -1,30 +1,61 @@
-(function( $ ) {
-    $.fn.Rangeslider = function(data) {
-        this.options = {
-            thumb2: false,
-            target: this,
-        }
-
-        let build = function(options) {
-            let _this = this;
-            _this.rsWrapper = $("<div/>").appendTo(options.target);
-            _this.rsWrapper.addClass("rsWrapper");
-
-            _this.rsBase = $("<div/>").appendTo(rsWrapper);
-            _this.rsBase.addClass("rsBase");
-
-            _this.rsThumb1 = $("<div/>").appendTo(rsBase);
-            _this.rsThumb1.addClass("rsThumb .rsThumb_1");
-
-            if (options.thumb2 == true) {
-                _this.rsThumb2 = $("<div/>").appendTo(rsBase);
-                _this.rsThumb2.addClass("rsThumb.rsThumb_2");
+(function($) {
+    $.fn.extend({
+        rangeslider: function(options) {
+            this.defaultOptions = {
+                additionThumb:        false,
+                target:               $(this),
+                thumbPosition:        0,
+                additionPosition:     100, 
+                min:                  0,
+                max:                  0,
+                description:          false,
             }
-        }
 
-        build(this.options);
-    }
+            var settings = $.extend({}, this.defaultOptions, options);
+
+            function rsBuild() {
+                let _this = this;
+                _this.rsWrapper = $("<div/>").appendTo(settings.target);
+                _this.rsWrapper.addClass("rsWrapper");
+            
+                _this.rsBase = $("<div/>").appendTo(rsWrapper);
+                _this.rsBase.addClass("rsBase");
+            
+                _this.rsThumb1 = $("<div/>").appendTo(rsBase);
+                _this.rsThumb1.addClass("rsThumb rsThumb_1");
+                _this.rsThumb1.css("left", settings.thumbPosition)
+
+            
+                if (settings.additionThumb == true) {
+                    _this.rsThumb2 = $("<div/>").appendTo(rsBase);
+                    _this.rsThumb2.addClass("rsThumb rsThumb_2");
+                    _this.rsThumb2.css("left", settings.additionPosition)
+                }
+            }
+
+            function rsModel() {
+
+            }
+
+            function rsConstructor() {
+                let baseWidth = rsBase.css("width");
+                
+            }
+
+            return this.each(function() {
+                var $this = $(this);
+                
+                rsBuild($this);
+                rsModel($this);
+                rsConstructor($this);
+            });
+        }
+    });
 })(jQuery);
 
 
-var $rangeslider = $(".test").Rangeslider()
+var $rangeslider = $(".test").rangeslider({
+    additionThumb: true,
+    thumbPosition: "10px",
+    additionPosition: "100px",
+})
